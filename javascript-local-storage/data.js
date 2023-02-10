f2a34a14326b89b657f5fa2bae8afb5ba9d81e9e
Kevin Lay <kevinlay@live.com>
2023-02-10 19:36:21 +0000

    Completed javascript-local-storagge exercise.

---
+++ javascript-local-storage/data.js
@@ -0,0 +1,7 @@
+/* exported todos */
+
+var todos = [];
+var previousTodosJSON = localStorage.getItem('javascript-local-storage');
+if (previousTodosJSON !== null) {
+  todos = JSON.parse(previousTodosJSON);
+}
