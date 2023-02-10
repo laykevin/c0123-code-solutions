d0be0218ca8bfb5bbc190a3937ba796179644734
Kevin Lay <kevinlay@live.com>
2023-02-10 22:35:59 +0000

    Completed javascript-typing-tutor bonus challenges.

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,10 +1,18 @@
 var $text = document.querySelectorAll('span');
+var $accuracy = document.querySelector('em');
+var $accuracy2 = document.querySelector('.accuracy2');
+var $msg = document.querySelector('h1');
+var $afterGame = document.querySelector('.hide2');
 var i = 0;
+var count = 0;
 function help(event) {
+  count++;
   if (event.key === $text[i].textContent) {
     if (i === $text.length - 1) {
       $text[i].className = 'green';
       i++;
+      $accuracy2.textContent = Math.round(($text.length / count) * 100);
+      $accuracy.textContent = Math.round(($text.length / count) * 100);
       return playAgain();
     }
     $text[i].className = 'green';
@@ -17,6 +25,12 @@ function help(event) {
 document.addEventListener('keydown', help);
 var $playAgain = document.querySelector('.hide');
 function playAgain(event) {
+  if (Math.round(($text.length / count) * 100) < 70) {
+    $msg.textContent = 'You Suck!';
+  }
+  if (Math.round(($text.length / count) * 100) >= 70) {
+    $msg.textContent = 'You did it!';
+  }
   $playAgain.className = 'row';
 }
 
@@ -24,6 +38,7 @@ var $noButton = document.querySelector('.no');
 $noButton.addEventListener('click', noButton);
 function noButton(event) {
   $playAgain.className = 'hide';
+  $afterGame.className = 'row2';
 }
 var $yesButton = document.querySelector('.yes');
 $yesButton.addEventListener('click', yesButton);
@@ -34,4 +49,9 @@ function yesButton(event) {
   $text[0].className = 'underline';
   $playAgain.className = 'hide';
   i = 0;
+  count = 0;
+  $afterGame.className = 'hide2';
 }
+
+var $restartButton = document.querySelector('.restart');
+$restartButton.addEventListener('click', yesButton);
