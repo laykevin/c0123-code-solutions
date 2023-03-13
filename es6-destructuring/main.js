81e8492a303c15da5c6b4b73356373633c905476
Kevin Lay <kevinlay@live.com>
2023-03-13 21:17:44 +0000

    Updated console log to use interpolation.

--- es6-destructuring/main.js
+++ es6-destructuring/main.js
@@ -5,7 +5,7 @@ const book1 = {
 };
 
 const { title, author, libraryID } = book1;
-console.log('The title of the book is', title + ',', 'the author is', author + ',', 'and the library id is', libraryID);
+console.log(`The title of the book is ${title}, the author is ${author}, and the library id is ${libraryID}`);
 
 const book2 = {
   title: 'Les Fleurs du mal',
@@ -14,7 +14,7 @@ const book2 = {
 };
 
 const { title: book2Title, author: book2Author, libraryID: book2LibraryID } = book2;
-console.log('The title of the book is', book2Title + ',', 'the author is', book2Author + ',', 'and the library id is', book2LibraryID);
+console.log(`The title of the book is ${book2Title}, the author is ${book2Author}, and the library id is ${book2LibraryID}`);
 
 const library = [
   {
