45a97784a06bf412dbc9595d8ad62cb58a87c789
Kevin Lay <kevinlay@live.com>
2023-04-01 17:29:14 +0000

    Completed react-accordion-component challenge.

---
+++ react-accordion-component/my-app/src/Accordion.js
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+export default function Accordion({ topics }) {
+  const [activeIndex, setActiveIndex] = useState();
+  function handleOnClick(index) {
+    index === activeIndex ? setActiveIndex() : setActiveIndex(index)
+  }
+
+  return <Panel topics={topics} onClick={handleOnClick} currentIndex={activeIndex}/>
+}
+
+function Panel({ topics, onClick, currentIndex }) {
+  const topicsLists = topics.map((topic, index) => (
+  <>
+    <h2 onClick={() => onClick(index)}>{topic.title}</h2>
+    {currentIndex === index && <p>{topic.text}</p>}
+  </>)
+  );
+  return (
+    <div>
+      {topicsLists}
+    </div>
+  )
+}
