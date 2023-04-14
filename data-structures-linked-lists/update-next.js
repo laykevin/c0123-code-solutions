7283b7c15dfc5c2bc76d9df5d09d9bc656b84b91
Kevin Lay <kevinlay@live.com>
2023-04-14 16:03:11 +0000

    Cleaned up code.

--- data-structures-linked-lists/update-next.js
+++ data-structures-linked-lists/update-next.js
@@ -1,10 +1,13 @@
-import LinkedList from './lib/linked-list';
+// import LinkedList from './lib/linked-list';
 
 export default function updateNext(list, value) {
-  const node = new LinkedList(value);
-  const current = list;
-  if (current.next !== null) {
-    node.next = current.next.next;
-    current.next = node;
+  // const node = new LinkedList(value);
+  // const current = list;
+  // if (current.next !== null) {
+  //   node.next = current.next.next;
+  //   current.next = node;
+  // }
+  if (list.next !== null) {
+    list.next.data = value;
   }
 }
